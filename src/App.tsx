155c5395b99f05dd78f785617573b828812f52d7
filiphelikar <filip.helikar@seznam.cz>
2024-07-19 12:53:01 +0200

    importing Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import style from "./App.module.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <Router>
-      {/* nav */}
+      <Navbar />
       <Routes>
         <Route path="/" element={<div>uvod</div>} />
         <Route path="/kocky" element={<div>kocky</div>} />
