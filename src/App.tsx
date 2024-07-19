39fbd723090b9d4087d8928e070c80cf73daa536
filiphelikar <filip.helikar@seznam.cz>
2024-07-19 12:49:00 +0200

    basic app html structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,17 @@ import style from "./App.module.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      {/* nav */}
+      <Routes>
+        <Route path="/" element={<div>uvod</div>} />
+        <Route path="/kocky" element={<div>kocky</div>} />
+        <Route path="/pes" element={<div>pes</div>} />
+      </Routes>
+      {/* footer */}
+    </Router>
+  );
 };
 
 export default App;
