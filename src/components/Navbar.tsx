c744a0938b5f0a5e13118b6291ea4d3851a24a21
filiphelikar <filip.helikar@seznam.cz>
2024-07-19 12:59:44 +0200

    basic html structure for Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,21 @@
 import style from "./Navbar.module.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className={style["nav"]}>
+      {/* TODO logo to="/" */}
+      <div className={style["link-container"]}>
+        <Link to="/">Uvod</Link>
+      </div>
+      <div className={style["link-container"]}>
+        <Link to="/kocky">Kocky</Link>
+      </div>
+      <div className={style["link-container"]}>
+        <Link to="/pes">Pes</Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
