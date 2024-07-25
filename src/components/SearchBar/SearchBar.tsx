4e43b3e9bae9bc1cb054ca01ab477aa1f8afcc00
filiphelikar <filip.helikar@seznam.cz>
2024-07-25 15:16:21 +0200

    SearchBar CSS

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import style from "./SearchBar.module.css";
 
 type Props = {
   setState: React.Dispatch<React.SetStateAction<string>>;
@@ -8,10 +9,10 @@ type Props = {
 const SearchBar: React.FC<Props> = ({ setState, url }) => {
   const [input, setInput] = useState<string>("");
   return (
-    <>
+    <div className={style["container"]}>
       <input type="text" onChange={(e) => setInput(e.target.value)} />
       <button onClick={() => setState(url + input)}>Vyhledat</button>
-    </>
+    </div>
   );
 };
 
