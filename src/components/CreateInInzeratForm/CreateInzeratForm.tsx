db6e2a0147fd73b01875744f967b3183d74f4abc
filiphelikar <filip.helikar@seznam.cz>
2024-07-27 14:41:10 +0200

    update inzerat form & css inzerat form

--- src/components/CreateInInzeratForm/CreateInzeratForm.tsx
+++ src/components/CreateInInzeratForm/CreateInzeratForm.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import style from "./CreateInzeratForm.module.css";
+import { BiHide, BiShow } from "react-icons/bi";
 
 type FormValues = {
   nadpis: string;
@@ -9,9 +10,10 @@ type FormValues = {
   cenaNum: number;
   prodejce: string;
   telefon: string;
-  email: string;
   lokalita: string;
   psc: string;
+  email: string;
+  heslo: string;
 };
 
 interface Props {
@@ -19,7 +21,9 @@ interface Props {
 }
 
 const CreateInzeratForm = ({ id }: Props) => {
+  const druh = id;
   const [showAdditionalField, setShowAdditionalField] = useState(false);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
   //const [message, setMessage] = useState<any>({message: "", class: "", display: 0})
 
   const form = useForm<FormValues>({
@@ -29,9 +33,10 @@ const CreateInzeratForm = ({ id }: Props) => {
       cenaSelect: "Vyberte",
       prodejce: "",
       telefon: "",
-      email: "",
       lokalita: "",
       psc: "",
+      email: "",
+      heslo: "",
     },
   });
 
@@ -52,13 +57,21 @@ const CreateInzeratForm = ({ id }: Props) => {
   const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} noValidate>
+    <form
+      className={style["main"]}
+      onSubmit={handleSubmit(onSubmit)}
+      noValidate
+    >
       <div className={style["form-1"]}>
-        <label htmlFor="nadpis">
+        <label className={style["label"]} htmlFor="nadpis">
           Nadpis:<span style={{ color: "red" }}>*</span>
         </label>
-        <p style={{ color: "red" }}>{errors.nadpis?.message}</p>
+        <p className={style["error-message"]} style={{ color: "red" }}>
+          {errors.nadpis?.message}
+        </p>
+        <br />
         <input
+          className={style["input"]}
           id="nadpis"
           type="text"
           placeholder="Aa"
@@ -70,11 +83,15 @@ const CreateInzeratForm = ({ id }: Props) => {
           })}
         />
         <br />
-        <label htmlFor="popis">
+        <label className={style["label"]} htmlFor="popis">
           Popis:<span style={{ color: "red" }}>*</span>
         </label>
-        <p style={{ color: "red" }}>{errors.popis?.message}</p>
+        <p className={style["error-message"]} style={{ color: "red" }}>
+          {errors.popis?.message}
+        </p>
+        <br />
         <textarea
+          className={style["textarea"]}
           id="popis"
           placeholder="Aa"
           {...register("popis", {
@@ -86,13 +103,19 @@ const CreateInzeratForm = ({ id }: Props) => {
         />
         <br />
 
-        {!showAdditionalField && (
-          <p style={{ color: "red" }}>{errors.cenaSelect?.message}</p>
-        )}
-
-        <label htmlFor="cenaSelect">
+        <label className={style["label"]} htmlFor="cenaSelect">
           Cena:<span style={{ color: "red" }}>*</span>
         </label>
+        {!showAdditionalField && (
+          <p className={style["error-message"]} style={{ color: "red" }}>
+            {errors.cenaSelect?.message}
+          </p>
+        )}
+        {!showAdditionalField && (
+          <>
+            <br />
+          </>
+        )}
         <select
           id="cenaSelect"
           {...register("cenaSelect", {
@@ -104,11 +127,16 @@ const CreateInzeratForm = ({ id }: Props) => {
           <option value="Za odvoz">Za odvoz</option>
           <option value="Číslo v Kč:">Číslo v Kč:</option>
         </select>
+        <br />
 
         {showAdditionalField && (
           <>
-            <p style={{ color: "red" }}>{errors.cenaNum?.message}</p>
+            <p className={style["error-message"]} style={{ color: "red" }}>
+              {errors.cenaNum?.message}
+            </p>
+            <br />
             <input
+              className={style["input"]}
               id="cenaNum"
               type="number"
               {...register("cenaNum", {
@@ -123,11 +151,15 @@ const CreateInzeratForm = ({ id }: Props) => {
       </div>
       <h2>Osobní údaje:</h2>
       <div className={style["form-2"]}>
-        <label htmlFor="prodejce">
-          Celé jméno:<span style={{ color: "red" }}>*</span>
+        <label className={style["label"]} htmlFor="prodejce">
+          Jméno:<span style={{ color: "red" }}>*</span>
         </label>
-        <p style={{ color: "red" }}>{errors.prodejce?.message}</p>
+        <p className={style["error-message"]} style={{ color: "red" }}>
+          {errors.prodejce?.message}
+        </p>
+        <br />
         <input
+          className={style["input"]}
           id="prodejce"
           type="text"
           placeholder="Aa"
@@ -139,11 +171,15 @@ const CreateInzeratForm = ({ id }: Props) => {
           })}
         />
         <br />
-        <label htmlFor="telefon">
+        <label className={style["label"]} htmlFor="telefon">
           Telefon:<span style={{ color: "red" }}>*</span>
         </label>
-        <p style={{ color: "red" }}>{errors.telefon?.message}</p>
+        <p className={style["error-message"]} style={{ color: "red" }}>
+          {errors.telefon?.message}
+        </p>
+        <br />
         <input
+          className={style["input"]}
           id="telefon"
           type="text"
           placeholder="Aa"
@@ -155,11 +191,55 @@ const CreateInzeratForm = ({ id }: Props) => {
           })}
         />
         <br />
-        <label htmlFor="email">
+        <label className={style["label"]} htmlFor="lokalita">
+          Lokalita:<span style={{ color: "red" }}>*</span>
+        </label>
+        <p className={style["error-message"]} style={{ color: "red" }}>
+          {errors.lokalita?.message}
+        </p>
+        <br />
+        <input
+          className={style["input"]}
+          id="lokalita"
+          type="text"
+          placeholder="Aa"
+          {...register("lokalita", {
+            required: {
+              value: true,
+              message: "Lokalita je povinná",
+            },
+          })}
+        />
+        <br />
+        <label className={style["label"]} htmlFor="psc">
+          Psč:<span style={{ color: "red" }}>*</span>
+        </label>
+        <p className={style["error-message"]} style={{ color: "red" }}>
+          {errors.psc?.message}
+        </p>
+        <br />
+        <input
+          className={style["input"]}
+          id="psc"
+          type="text"
+          placeholder="Aa"
+          {...register("psc", {
+            required: {
+              value: true,
+              message: "Psč je povinné",
+            },
+          })}
+        />
+        <br />
+        <label className={style["label"]} htmlFor="email">
           E-mail:<span style={{ color: "red" }}>*</span>
         </label>
-        <p style={{ color: "red" }}>{errors.email?.message}</p>
+        <p className={style["error-message"]} style={{ color: "red" }}>
+          {errors.email?.message}
+        </p>
+        <br />
         <input
+          className={style["input"]}
           id="email"
           type="email"
           placeholder="Aa"
@@ -176,37 +256,44 @@ const CreateInzeratForm = ({ id }: Props) => {
           })}
         />
         <br />
-        <label htmlFor="lokalita">
-          Lokalita:<span style={{ color: "red" }}>*</span>
+        <label className={style["label"]} htmlFor="heslo">
+          Heslo:<span style={{ color: "red" }}>*</span>
         </label>
-        <p style={{ color: "red" }}>{errors.lokalita?.message}</p>
-        <input
-          id="lokalita"
-          type="text"
-          placeholder="Aa"
-          {...register("lokalita", {
-            required: {
-              value: true,
-              message: "Lokalita je povinná",
-            },
-          })}
-        />
+        <p className={style["error-message-password"]} style={{ color: "red" }}>
+          {errors.heslo?.message}
+        </p>
         <br />
-        <label htmlFor="psc">
-          Psč:<span style={{ color: "red" }}>*</span>
-        </label>
-        <p style={{ color: "red" }}>{errors.psc?.message}</p>
-        <input
-          id="psc"
-          type="text"
-          placeholder="Aa"
-          {...register("psc", {
-            required: {
-              value: true,
-              message: "Psč je povinné",
-            },
-          })}
-        />
+        {errors.heslo?.message ===
+          "Heslo musí obsahovat alespoň jedno malé písmeno, jedno velké písmeno, jedno číslo, jeden speciální znak a musí mít minimálně 8 znaků" && (
+          <br />
+        )}
+        <div className={style["password-container"]}>
+          <button
+            type="button"
+            className={style["show-btn"]}
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? <BiShow /> : <BiHide />}
+          </button>
+          <input
+            className={style["input"]}
+            id="heslo"
+            type={showPassword ? "text" : "password"}
+            placeholder="Aa"
+            {...register("heslo", {
+              required: {
+                value: true,
+                message: "Heslo je povinné",
+              },
+              pattern: {
+                value:
+                  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                message:
+                  "Heslo musí obsahovat alespoň jedno malé písmeno, jedno velké písmeno, jedno číslo, jeden speciální znak a musí mít minimálně 8 znaků",
+              },
+            })}
+          />
+        </div>
       </div>
       <input type="submit" />
     </form>
