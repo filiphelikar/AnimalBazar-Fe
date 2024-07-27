6e72d78b34fc4295f757060dd598e8c76f1564a1
filiphelikar <filip.helikar@seznam.cz>
2024-07-27 19:56:49 +0200

    usePost

---
+++ src/utils/usePost.ts
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+type FetchStatus = "loading" | "success" | "error";
+
+const usePostRequest = <T>(url: string) => {
+  const [data, setData] = useState<T | null>(null);
+  const [status, setStatus] = useState<FetchStatus>("loading");
+
+  const postRequest = async (postData: T) => {
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const responseData = await response.json();
+      setStatus("success");
+      setData(responseData);
+    } catch (error) {
+      setStatus("error");
+    }
+  };
+
+  return { data, status, postRequest };
+};
+
+export default usePostRequest;
