f9c570decef1322a3056b6717ca7ee0420fbaf15
filiphelikar <filip.helikar@seznam.cz>
2024-08-10 16:27:28 +0200

    handling search with no results

--- src/components/Search/SearchWithResult.tsx
+++ src/components/Search/SearchWithResult.tsx
@@ -5,6 +5,7 @@ import LoadingError from '../LoadingError/LoadingError';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import SearchBar from './SearchBar';
+import style from './SearchWithResult.module.css';
 
 const SearchWithResult = () => {
   const { id } = useParams();
@@ -17,6 +18,7 @@ const SearchWithResult = () => {
   }
 
   const [submitInput, setSubmitInput] = useState<string>(url);
+  const [message, setMessage] = useState<string | null>(null);
 
   const { data, status } = useFetch<Inzerat[]>(submitInput);
 
@@ -26,11 +28,23 @@ const SearchWithResult = () => {
     }, [id]);
   }
 
+  useEffect(() => {
+    if (data !== null && !data[0]) {
+      setMessage('Žádné výsledky hledání :(');
+      setSubmitInput(url);
+    } else if (submitInput != url) {
+      setMessage(null);
+    }
+  }, [data]);
+
   return (
     <>
       <SearchBar setState={setSubmitInput} url={`http://localhost:3000/api/search?${druh}param=`} />
       {data && status === 'success' ?
-        <InzeratyTable inzeraty={data} />
+        <>
+          <p className={style['error-message']}>{message}</p>
+          <InzeratyTable inzeraty={data} />{' '}
+        </>
       : <LoadingError status={status} />}
     </>
   );
