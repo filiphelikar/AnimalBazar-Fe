989aa37b0f2432423a720358cab8224bdf08016d
filiphelikar <filip.helikar@seznam.cz>
2024-08-11 19:27:23 +0200

    responsible Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,9 @@
+import { useEffect, useState } from 'react';
 import style from './Navbar.module.css';
 import { Link } from 'react-router-dom';
 import { PiCatDuotone } from 'react-icons/pi';
 import ThemeButton from '../ThemeButton/ThemeButton';
-import { SetStateAction } from 'react';
+import { FaBars } from 'react-icons/fa';
 
 interface Props {
   druhy: string[];
@@ -11,19 +12,47 @@ interface Props {
 }
 
 const Navbar: React.FC<Props> = ({ druhy, setTheme, theme }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className={style['nav']}>
       <div className={style['nav-section-1']}>
-        <Link className={style['logo']} to='/'>
+        {windowWidth <= 550 && <ThemeButton setTheme={setTheme} theme={theme} />}
+        <Link onClick={() => setMenuOpen(false)} className={style['logo']} to='/'>
           <PiCatDuotone />
         </Link>
-        {druhy.map((druh: string) => {
-          return (
-            <Link to={`/inzeraty/${druh}`} key={druh} className={style['link-container']}>
+        <div className={style['menu-icon']} onClick={toggleMenu}>
+          <FaBars />
+        </div>
+        <div className={`${style['links-container']} ${menuOpen ? style['show'] : ''}`}>
+          {menuOpen && windowWidth <= 550 && (
+            <Link onClick={toggleMenu} to={'/vytvořit-inzerat'} className={style['link-container']}>
+              <p>vytvořit inzerát</p>
+            </Link>
+          )}
+          {druhy.map((druh: string) => (
+            <Link onClick={toggleMenu} to={`/inzeraty/${druh}`} key={druh} className={style['link-container']}>
               <p>{druh}</p>
             </Link>
-          );
-        })}
+          ))}
+        </div>
       </div>
       <div className={style['nav-section-2']}>
         <Link to={'/vytvořit-inzerat'} className={style['link-container']}>
